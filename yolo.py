0779d6991d2f73f6e000f2e976fe57f7495bb11c
Monjurul <raju.monjurulkarim@gmail.com>
2021-08-13 19:17:29 -0400

    updated the font/resource

--- yolo.py
+++ yolo.py
@@ -130,13 +130,14 @@ class YOLO(object):
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
 
-        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
+        font = ImageFont.truetype(font='arial.ttf',
                     size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+
+        #If the above font does not work for you please uncomment the following line
+        # font = ImageFont.load_default()
         thickness = (image.size[0] + image.size[1]) // 300
 
-        # rectangle_points = ((170.5,552.5), (655.5, 552.5), (655.5, 312.5),(170.5, 312.5))
-        # draw = ImageDraw.Draw(image, 'RGBA')
-        # draw.polygon((rectangle_points), fill= (255, 255, 0, 40), outline =(255,255,0))
+
         area = 0
         max_area = 0
         max_left = 0
@@ -184,7 +185,7 @@ class YOLO(object):
         # if max_area != 0:
         ##using car_original_width
         car_original_width = 72 # in inch
-        car_original_height = 60
+        car_original_height = 60 # in inch
         f = 250 # focal length
         # d = (car_original_width * f)/ width
         d = (car_original_height * f)/ height
@@ -209,49 +210,6 @@ class YOLO(object):
     def close_session(self):
         self.sess.close()
 
-    '''
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = self.class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
-
-
-            label = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
-
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-            print(label, (left, top), (right, bottom))
-
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
-            else:
-                text_origin = np.array([left, top + 1])
-
-            # My kingdom for a good redistributable image drawing library.
-            if predicted_class != 'person':
-                for i in range(thickness):
-                    draw.rectangle(
-                        [left + i, top + i, right - i, bottom - i],
-                        outline=self.colors[c])
-                draw.rectangle(
-                    [tuple(text_origin), tuple(text_origin + label_size)],
-                    fill=self.colors[c])
-                draw.text(text_origin, label, fill=(0, 0, 0), font=font)
-                del draw
-
-        end = timer()
-        print(end - start)
-        return image
-
-    def close_session(self):
-        self.sess.close()
-
-    '''
 
 
 def detect_video(yolo, video_path, output_path=""):
